3483aa030682718cb2976001293d9778f1eb49f4
Jorge Burgos <jburgos@applaudostudios.com>
2017-08-10 11:31:54 -0600

    Updates models to use postgres and reduces shown endpoints in explorer

--- common/models/user.js
+++ common/models/user.js
@@ -1,19 +1,25 @@
 'use strict';
 
-module.exports = function(user) {
-  user.disableRemoteMethodByName('createChangeStream');
-  user.disableRemoteMethodByName('replaceOrCreate');
-  user.disableRemoteMethodByName('replaceById');
-  user.disableRemoteMethodByName('findOne');
-  user.disableRemoteMethodByName('upsert');
-  user.disableRemoteMethodByName('updateAll');
-  user.disableRemoteMethodByName('upsertWithWhere');
+const methods = [
+  'prototype.__get__accessTokens',
+  'prototype.__count__accessTokens',
+  'prototype.__create__accessTokens',
+  'prototype.__findById__accessTokens',
+  'prototype.__delete__accessTokens',
+  'prototype.__destroyById__accessTokens',
+  'prototype.__updateById__accessTokens',
+  'prototype.__get__orders',
+  'prototype.__count__orders',
+  'prototype.__create__orders',
+  'prototype.__findById__orders',
+  'prototype.__delete__orders',
+  'prototype.__destroyById__orders',
+  'prototype.__updateById__orders',
+  'prototype.__unlink__orders',
+  'prototype.__link__orders',
+  'prototype.__exists__orders'
+]
 
-  user.disableRemoteMethodByName('prototype.__count__accessTokens');
-  user.disableRemoteMethodByName('prototype.__create__accessTokens');
-  user.disableRemoteMethodByName('prototype.__delete__accessTokens');
-  user.disableRemoteMethodByName('prototype.__destroyById__accessTokens');
-  user.disableRemoteMethodByName('prototype.__findById__accessTokens');
-  user.disableRemoteMethodByName('prototype.__get__accessTokens');
-  user.disableRemoteMethodByName('prototype.__updateById__accessTokens');
+module.exports = function(User) {
+  methods.map(methodName => User.disableRemoteMethodByName(methodName))
 };
